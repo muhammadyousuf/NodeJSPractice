61d3351f91a5fa6091c545da0323512990d66fca
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-01-26 20:11:50 +0500

    Add unique array function

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ const node = require("./node.js")
 var res = node.nodeName();
 console.log(res)
 
-console.log("Add Number:"+ node.add(5,3));
-console.log("Subtraction Number:"+ node.sub(5,3));
-console.log("Multiplication Number:"+ node.mul(5,3));
-console.log("Division Number:"+ node.div(5,3));
-console.log("Remainder Number:"+ node.remainder(5,3));
+// console.log("Add Number:"+ node.add(5,3));
+// console.log("Subtraction Number:"+ node.sub(5,3));
+// console.log("Multiplication Number:"+ node.mul(5,3));
+// console.log("Division Number:"+ node.div(5,3));
+// console.log("Remainder Number:"+ node.remainder(5,3));
 
-console.log(_.isString(true));
-console.log(_.isString("Muhammad Yousuf"));
+// console.log(_.isString(true));
+// console.log(_.isString("Muhammad Yousuf"));
 //var user = os.userInfo();
 //console.log("user", user);
 
 //fs.appendFileSync('User.txt', `Hello ${user.username}! Your age is ${node.age}`);
+
+var FilterArray = _.uniq(["Muhammad",3,5,2,4,3,"Muhammad",5])
+console.log(FilterArray)
