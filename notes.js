4f9d79ecfcc3d49af809afb285ab9b20004b1d54
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-09 12:59:36 +0500

    add notes function completed

--- notes.js
+++ notes.js
@@ -1,8 +1,27 @@
 console.log("Notes Its Working");
 
+const fs = require('fs');
 addNote = (title, body) => {
-    console.log("Add Notes", title, body);
+    var nodes = [];
+    var node = {
+        title,
+        body
+    }
+    try{
+    var getdata = fs.readFileSync('node-data.json');
+    nodes = JSON.parse(getdata);
+    }catch(e){
+
+    }
+    var duplicate = nodes.filter((note) => note.title === title );
+    if(duplicate.length === 0){
+    nodes.push(node);
+    fs.writeFileSync('node-data.json', JSON.stringify(nodes));
+    }
+
 }
+
+
 getAll = () => {
     console.log("Get All Notes");
 }
